25a4ca1e48b4e404fa81404fdf153c9994fc17af
Jacci <jacciziebert@gmail.com>
2020-03-25 11:15:03 -0500

    county code / name input

--- server.R
+++ server.R
@@ -1,8 +1,12 @@
 library(dplyr)
 library(ggplot2)
+library(DT)
+library(expss)
 
-setwd("W:/HSSA/Keep/Jaclyn Ziebert/R_WD_Data/R_Shiny")
+setwd("W:/HSSA/Keep/Jaclyn Ziebert/R_WD_Data/Shiny_Crashes")
 # all_crashes <- read.csv("data/2020_crash.csv")
+county_recode <- read.csv("data/county_recode.csv")
+# setNames(county_recode$CountyCode, county_recode$CountyName)
 
 server <- function(input, output, session) {
   output$userpanel <- renderUI({
@@ -12,22 +16,32 @@ server <- function(input, output, session) {
     }
   })
   
-  crash_jan <- reactive({              # INPUTS: user selects from county list
-    all_crashes%>%filter(
-      CNTYCODE %in% input$cntynum)
-      })
+  # crash_month <- reactive({              # INPUTS: user selects from county list
+  #   all_crashes%>%filter(
+  #     CNTYCODE %in% input$cntynum)
+  #     })
+  # 
+  updatePickerInput(session, "cntynum", choices = setNames(county_recode$CountyCode, county_recode$CountyName))
   
-  output$curve_plot <- renderPlot({    # OUTPUTS: test to show plot
-    curve(x ^ input$exponent, from = -5, to = 5)
+  output$biketable <- renderDT({
+    all_crashes %>% 
+      tab_cells(CNTYCODE) %>%       # stuff to put in the rows
+      tab_subgroup(ALCFLAG == "Yes") %>%                # only select certain elements
+      tab_cols(CRSHSVR %nest% ALCFLAG, total()) %>%     # columns with nesting
+      tab_stat_cases(total_label = "Total Crashes") %>% # frequency count, can also do percent
+      tab_pivot() %>%
+      drop_empty_columns() %>% 
+      datatable(rownames = FALSE)
   })
   
+  
   output$bikeflag <- renderPlot({
-    # all_crashes <- rbind(crash_jan())  #take variable of what was inputted
+    # all_crashes <- rbind(crash_month())  #take variable of what was inputted
     # all_crashes$group <- c()
     
     all_crashes <- all_crashes %>% 
       # group_by(CRSHMTH) %>%
-      filter(BIKEFLAG == "Y")
+      filter(BIKEFLAG == "Y", CNTYCODE == input$cntynum)  #CNTYCODE is what changes chart
     
     all_crashes %>% 
     ggplot() +
@@ -36,17 +50,7 @@ server <- function(input, output, session) {
   
 }
 
-# shinyApp(ui, server)
-
-
-# function(input, output, session) {
-#   
-  # output$curve_plot <- renderPlot({
-  #   curve(x ^ input$exponent, from = -5, to = 5)
-  # })
-#   
-# }
-# input - from widgetse, controls, never include variables likes input$var
+# input - from widgets, controls, never include variables likes input$var
 # output - stuff to send to ui, always quite variables, plotOutput("plot")
 # plot charts uses renderPlot and plotOutput
 # text is renderText and textOutput
--- ui.R
+++ ui.R
@@ -2,46 +2,26 @@
 #charts must be rendered in server file then displayed in outpur fct
 library(shinydashboard)
 library(shinyWidgets)
-#
-county_list = c(2,34,35)
+library(DT)
+
+county_list = c(2, 13, 34,35)
 
 ui <- dashboardPage(
   dashboardHeader(title = "My first app"),
   
-  dashboardSidebar(                                     # SIDEBAR
+  dashboardSidebar(                                 # SIDEBAR
     # The dynamically-generated user panel
     # uiOutput("userpanel")
-    pickerInput("cntynum", "County", sort(as.character(county_list)), options = list("actions-box" = TRUE), multiple = FALSE, selected="34")),
-    
+    pickerInput("cntynum", "County",
+                choices = sort(unique(all_crashes$CNTYCODE)),
+                options = list("actions-box" = TRUE),
+                multiple = FALSE,
+                selected = "42")  # this does not work after I set in cnty names
+    ),
   dashboardBody(                                    # BODY
-      sliderInput(
-        "exponent",
-        label = "Choose 1",
-        min = 1,
-        max = 5,
-        value = 2
-      ),
-      plotOutput("curve_plot"),
       
-      plotOutput("bikeflag")
+      plotOutput("bikeflag"),
+      
+      DTOutput("biketable")
     )
   )
-
-restr
-# dashboardPage(
-#   dashboardHeader(
-# 
-#   title = "My first app"),
-#   # tabPanel("crash")),
-#   
-#   
-#   dashboardBody(
-    
-  # sliderInput("exponent",
-  #             label = "Choose 1",
-  #             min = 1,
-  #             max = 5,
-  #             value = 2),
-  # plotOutput("curve_plot")
-# ))
-# navBar  - can use other layouts
